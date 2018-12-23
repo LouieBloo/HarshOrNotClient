76811ae9701e4d63321a87e4b9980a2f626f6d12
Luke <laleggio@ucdavis.edu>
2018-12-23 14:37:38 -0800

    Redid login page and added remember me button

--- src/app/components/pages/login/login.component.ts
+++ src/app/components/pages/login/login.component.ts
@@ -13,14 +13,34 @@ export class LoginComponent implements OnInit {
   loginDetails:TokenPayload = {email:"",password:""};
   error:string;
 
+  showingLoginInput:boolean = false;
+
+  rememberMeEmailKey:string = "rememberMeEmail";
+  rememberMe:boolean = false;
+
+  
+
   constructor(private auth:AuthService,private router: Router) { }
 
+  //see if there is an email stored
   ngOnInit() {
+    this.loadSavedEmail();
+  }
+
+
+
+  loginButtonClicked(){
+    if(this.showingLoginInput){
+      this.submit();
+    }else{
+      this.showingLoginInput = !this.showingLoginInput;
+    }
   }
 
   submit(){
     this.auth.login(this.loginDetails).subscribe(result =>{
       if(result.token && !this.error){
+        this.saveEmail();
         this.router.navigateByUrl('/');
       }else if(result.error){
         if(result.error.email){
@@ -30,6 +50,22 @@ export class LoginComponent implements OnInit {
         }
       }
     });
+  }
+
+  loadSavedEmail(){
+    let email = localStorage.getItem(this.rememberMeEmailKey);
+    if(email && email.length > 0){
+      this.rememberMe = true;
+      this.loginDetails.email = email;
+    }
+  }
+
+  saveEmail(){
+    if(this.rememberMe){
+      localStorage.setItem(this.rememberMeEmailKey,this.loginDetails.email);
+    }else{
+      localStorage.removeItem(this.rememberMeEmailKey);
+    }
   }
 
 }
